4e28b2e024a336aa96d818aeac82617762c65b67
joshua-8 <joshua_phelps@brown.edu>
2023-12-14 19:31:35 -0500

    before testing

--- drive-base/drive-base.ino
+++ drive-base/drive-base.ino
@@ -1,35 +1,49 @@
 #include <ESP32_easy_wifi_data.h> //https://github.com/joshua-8/ESP32_easy_wifi_data >=v1.0.0
 #include <JMotor.h> //https://github.com/joshua-8/JMotor
 
-// const int dacUnitsPerVolt = 413; // increasing this number decreases the calculated voltage
+const int dacUnitsPerVolt = 280; // increasing this number decreases the calculated voltage
+const byte batMonitorPin = 35;
+
 const int encoderTicksPerRev = 530;
 JTwoDTransform robotToWheelScalar = { 1, 1, 1 }; // adjust until it converts robot speed in your chosen units to wheel units (increasing numbers makes robot faster)
 
-#define flEncPins 36, 39
+#define blEncPins 36, 39
+#define flEncPins 19, 27
+#define frEncPins 22, 34
+#define brEncPins 23, 21 // one pin bad
 
-#define flMotPins 0, 32, 33, 8
+// byte _pinPosCh, byte _pinPos, byte _pinNeg, byte _pinNegCh
+#define blMotPins 8, 25, 26, 9
+#define flMotPins 10, 33, 32, 11
+#define frMotPins 12, 5, 18, 13
+#define brMotPins 14, 16, 17, 15
 
-// JVoltageCompMeasure<10> voltageComp = JVoltageCompMeasure<10>(batMonitorPin, dacUnitsPerVolt);
-JVoltageCompConst voltageComp = JVoltageCompConst(12);
-JMotorDriverEsp32HBridge flMotorDriver = JMotorDriverEsp32HBridge(flMotPins);
-// JMotorDriverEsp32L293 frMotorDriver = JMotorDriverEsp32L293(portB, true, false, false, 8000, 12);
-// JMotorDriverEsp32L293 blMotorDriver = JMotorDriverEsp32L293(portC, true, false, false, 8000, 12);
-// JMotorDriverEsp32L293 brMotorDriver = JMotorDriverEsp32L293(portD, true, false, false, 8000, 12);
-JEncoderQuadratureAttachInterrupt flEncoder = JEncoderQuadratureAttachInterrupt(flEncPins, 1.0 / encoderTicksPerRev, true, 20000);
-// JEncoderSingleAttachInterrupt frEncoder = JEncoderSingleAttachInterrupt(inport2, 1.0 / encoderTicksPerRev, false, 20000, 200, FALLING);
-// JEncoderSingleAttachInterrupt blEncoder = JEncoderSingleAttachInterrupt(inport3, 1.0 / encoderTicksPerRev, false, 20000, 200, FALLING);
-// JEncoderSingleAttachInterrupt brEncoder = JEncoderSingleAttachInterrupt(port3Pin, 1.0 / encoderTicksPerRev, false, 20000, 200, FALLING);
-// JMotorCompBasic motorCompensator = JMotorCompBasic(voltageComp, 1.7, 0.5); // volts per rps, min rps
-// JControlLoopBasic flCtrlLoop = JControlLoopBasic(10, 400);
-// JControlLoopBasic frCtrlLoop = JControlLoopBasic(10, 400);
-// JControlLoopBasic blCtrlLoop = JControlLoopBasic(10, 400);
-// JControlLoopBasic brCtrlLoop = JControlLoopBasic(10, 400);
-// JMotorControllerClosed flMotor = JMotorControllerClosed(flMotorDriver, motorCompensator, flEncoder, flCtrlLoop, INFINITY, INFINITY, 0.25;
-// JMotorControllerClosed frMotor = JMotorControllerClosed(frMotorDriver, motorCompensator, frEncoder, frCtrlLoop, INFINITY, INFINITY, 0.25);
-// JMotorControllerClosed blMotor = JMotorControllerClosed(blMotorDriver, motorCompensator, blEncoder, blCtrlLoop, INFINITY, INFINITY, 0.25);
-// JMotorControllerClosed brMotor = JMotorControllerClosed(brMotorDriver, motorCompensator, brEncoder, brCtrlLoop, INFINITY, INFINITY, 0.25);
-// JDrivetrainMecanum drivetrain = JDrivetrainMecanum(frMotor, flMotor, blMotor, brMotor, robotToWheelScalar); // drivetrain converts from robot speed to motor speeds
-// JDrivetrainControllerBasic drivetrainController = JDrivetrainControllerBasic(drivetrain, { INFINITY, INFINITY, INFINITY }, { 5, 5, 5 }, { 0.15, 0.15, 0.25 }, false);
+JVoltageCompMeasure<10> voltageComp = JVoltageCompMeasure<10>(batMonitorPin, dacUnitsPerVolt);
+
+JMotorDriverEsp32HBridge blMotorDriver = JMotorDriverEsp32HBridge(blMotPins, false, 2000, 12, false);
+JMotorDriverEsp32HBridge flMotorDriver = JMotorDriverEsp32HBridge(flMotPins, false, 2000, 12, false);
+JMotorDriverEsp32HBridge frMotorDriver = JMotorDriverEsp32HBridge(frMotPins, false, 2000, 12, false);
+JMotorDriverEsp32HBridge brMotorDriver = JMotorDriverEsp32HBridge(brMotPins, false, 2000, 12, false);
+
+JEncoderQuadratureAttachInterrupt blEncoder = JEncoderQuadratureAttachInterrupt(blEncPins, 1.0 / encoderTicksPerRev, true, 200000);
+JEncoderQuadratureAttachInterrupt flEncoder = JEncoderQuadratureAttachInterrupt(flEncPins, 1.0 / encoderTicksPerRev, true, 200000);
+JEncoderQuadratureAttachInterrupt frEncoder = JEncoderQuadratureAttachInterrupt(frEncPins, 1.0 / encoderTicksPerRev, true, 200000);
+JEncoderQuadratureAttachInterrupt brEncoder = JEncoderQuadratureAttachInterrupt(brEncPins, 1.0 / encoderTicksPerRev, true, 200000);
+
+JMotorCompBasic motorCompensator = JMotorCompBasic(voltageComp, 3, 0.5); // volts per rps, min rps
+
+JControlLoopBasic flCtrlLoop = JControlLoopBasic(30);
+JControlLoopBasic frCtrlLoop = JControlLoopBasic(30);
+JControlLoopBasic blCtrlLoop = JControlLoopBasic(30);
+JControlLoopBasic brCtrlLoop = JControlLoopBasic(30);
+
+JMotorControllerClosed flMotor = JMotorControllerClosed(flMotorDriver, motorCompensator, flEncoder, flCtrlLoop, INFINITY, INFINITY, 0.15);
+JMotorControllerClosed frMotor = JMotorControllerClosed(frMotorDriver, motorCompensator, frEncoder, frCtrlLoop, INFINITY, INFINITY, 0.15);
+JMotorControllerClosed blMotor = JMotorControllerClosed(blMotorDriver, motorCompensator, blEncoder, blCtrlLoop, INFINITY, INFINITY, 0.15);
+JMotorControllerClosed brMotor = JMotorControllerClosed(brMotorDriver, motorCompensator, brEncoder, brCtrlLoop, INFINITY, INFINITY, 0.15);
+
+JDrivetrainMecanum drivetrain = JDrivetrainMecanum(frMotor, flMotor, blMotor, brMotor, robotToWheelScalar); // drivetrain converts from robot speed to motor speeds
+JDrivetrainControllerBasic drivetrainController = JDrivetrainControllerBasic(drivetrain, { INFINITY, INFINITY, INFINITY }, { INFINITY, INFINITY, INFINITY }, { INFINITY, INFINITY, INFINITY }, false);
 
 boolean enabled = false;
 boolean wasEnabled = false;
@@ -42,51 +56,54 @@ JTwoDTransform driverInputWifi = JTwoDTransform();
 void Enabled()
 {
     // code to run while enabled, put your main code here
-    // if (mode == 1) { // drive
-    //     drivetrainController.moveVel(JDeadzoneRemover::calculate(driverInputWifi, { 0, 0, 0 }, drivetrainController.getMaxVel(), { .02, .02, .02 }));
-    // } else {
-    //     drivetrainController.moveVel({ 0, 0, 0 });
-    // }
+    if (mode == 1) { // drive
+        drivetrainController.moveVel(JDeadzoneRemover::calculate(driverInputWifi, { 0, 0, 0 }, drivetrainController.getMaxVel() * .85, { .02, .02, .02 }));
+    } else {
+        drivetrainController.moveVel({ 0, 0, 0 });
+    }
 }
 
 void Enable()
 {
     // turn on outputs
-    // drivetrainController.resetDist();
-    // drivetrainController.moveDist({ 0, 0, 0 });
-    // drivetrainController.enable();
+    drivetrainController.resetDist();
+    drivetrainController.moveDist({ 0, 0, 0 });
+    drivetrainController.enable();
     driverInputWifi = { 0, 0, 0 };
-    flMotorDriver.enable();
 }
 
 void Disable()
 {
     // shut off all outputs
-    // drivetrainController.disable();
+    drivetrainController.disable();
     driverInputWifi = { 0, 0, 0 };
-    flMotorDriver.disable();
 }
 
+jENCODER_MAKE_ISRS_MACRO(blEncoder);
 jENCODER_MAKE_ISRS_MACRO(flEncoder);
-// jENCODER_MAKE_ISRS_MACRO(frEncoder);
-// jENCODER_MAKE_ISRS_MACRO(brEncoder);
-// jENCODER_MAKE_ISRS_MACRO(blEncoder);
+jENCODER_MAKE_ISRS_MACRO(frEncoder);
+jENCODER_MAKE_ISRS_MACRO(brEncoder);
 void PowerOn()
 {
+    blMotorDriver.pwmDriverPos.disableState = HIGH;
+    blMotorDriver.pwmDriverNeg.disableState = HIGH;
     flMotorDriver.pwmDriverPos.disableState = HIGH;
     flMotorDriver.pwmDriverNeg.disableState = HIGH;
+    frMotorDriver.pwmDriverPos.disableState = HIGH;
+    frMotorDriver.pwmDriverNeg.disableState = HIGH;
+    brMotorDriver.pwmDriverPos.disableState = HIGH;
+    brMotorDriver.pwmDriverNeg.disableState = HIGH;
 
     // runs once on robot startup, set pin modes and use begin() if applicable here
+    blEncoder.setUpInterrupts(blEncoder_jENCODER_ISR_A, blEncoder_jENCODER_ISR_B);
     flEncoder.setUpInterrupts(flEncoder_jENCODER_ISR_A, flEncoder_jENCODER_ISR_B);
-    // frEncoder.setUpInterrupts(frEncoder_jENCODER_ISR_A, frEncoder_jENCODER_ISR_B);
-    // brEncoder.setUpInterrupts(brEncoder_jENCODER_ISR, brEncoder_jENCODER_ISR_B);
-    // blEncoder.setUpInterrupts(blEncoder_jENCODER_ISR, blEncoder_jENCODER_ISR_B);
+    frEncoder.setUpInterrupts(frEncoder_jENCODER_ISR_A, frEncoder_jENCODER_ISR_B);
+    brEncoder.setUpInterrupts(brEncoder_jENCODER_ISR_A, brEncoder_jENCODER_ISR_B);
 }
 
 void Always()
 {
-    // drivetrainController.run();
-    flMotorDriver.set(driverInputWifi.y);
+    drivetrainController.run();
     delay(1);
 }
 
@@ -112,9 +129,9 @@ void WifiDataToSend()
 {
     EWD::sendFl(voltageComp.getSupplyVoltage());
     // add data to send here: (EWD::sendBl(), EWD::sendBy(), EWD::sendIn(), EWD::sendFl())(boolean, byte, int, float)
-    EWD::sendFl(flEncoder.getPos());
-    EWD::sendFl(flEncoder.getVel());
-    EWD::sendFl(driverInputWifi.y);
+    EWD::sendFl(drivetrainController.getVel().x);
+    EWD::sendFl(drivetrainController.getVel().y);
+    EWD::sendFl(drivetrainController.getVel().theta);
 }
 
 ////////////////////////////// you don't need to edit below this line ////////////////////
